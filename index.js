f5f16b7558a7611cf18986751801d7be4b394188
Ramon Larivoir <rlarivoir@hotmail.com>
2019-09-05 21:28:44 -0300

    adicionando novas rotas

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const router = require("./router");
 const handlers = require("./handlers");
 
 const route = {};
+route["/"] = handlers.impares;
+route["/impares"] = handlers.impares;
 route["/impares.html"] = handlers.impares;
 route["404"] = handlers.notfound;
 
