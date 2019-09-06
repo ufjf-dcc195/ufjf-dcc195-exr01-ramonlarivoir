d320bf953078863653cf1fe280d73a7080d90b49
Ramon Larivoir <rlarivoir@hotmail.com>
2019-09-05 21:21:40 -0300

    cria função start

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 let http = require("http");
 const url = require("url");
 
+function start(route, routes) {
+    let server = http.createServer(function(req, res) {
+        route(url.parse(req.url).pathname, routes, req, res);
+    });
+    server.listen(8888);
+}
+
+exports.start = start;
