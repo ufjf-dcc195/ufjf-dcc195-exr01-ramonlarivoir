802809399e512807a9718a51cad2b8258771de23
Ramon Larivoir <rlarivoir@hotmail.com>
2019-09-05 21:16:33 -0300

    fecha res

--- handlers.js
+++ handlers.js
@@ -1,10 +1,11 @@
 exports.impares = function impares(req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});
-    res.writeHead("<ul>");
+    res.write("<ul>");
     for(let i = 1; i <= 100; i++) {
         if(i % 2 != 0) {
-            res.writeHead(`<li>${i}</li>`);
+            res.write(`<li>${i}</li>`);
         }
     }
-    res.writeHead("</ul>");
+    res.write("</ul>");
+    res.end();
 }
